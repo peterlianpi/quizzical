aeda5d3586ee75e5710c699f909966304c2cdc74
Peter <36624809+peterlianpi@users.noreply.github.com>
2023-08-24 23:23:13 +0630

    intro-page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as Blobs } from "./images/blobs.svg";
+import { ReactComponent as Blobs2 } from "./images/blobs2.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="intro-page">
+        <Blobs />
+        <Blobs2 />
+        <h2 className="header">Quizzical</h2>
+        <p className="description">Some description if needed</p>
+        <button className="start--button">Start quiz</button>
+      </div>
+    </main>
   );
 }
 
